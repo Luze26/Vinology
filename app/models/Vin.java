1fda3d6843d2b55e317ca9c60079188b49d1e5f4
Luze26 <yann.uzel@gmail.com>
2014-01-07 21:35:39 +0100

    properties on wine

--- app/models/Vin.java
+++ app/models/Vin.java
@@ -9,6 +9,12 @@ import play.Play;
 public class Vin {
 
     private Individual vinIndividual;
+    private String cepage = null;
+    private String cru = null;
+    private String cuvee = null;
+    private String sol = null;
+    private String degre = null;
+
 
     public Vin(Individual vinIndividual) {
         this.vinIndividual = vinIndividual;
@@ -19,8 +25,42 @@ public class Vin {
     }
 
     public String getCepage() {
-        Property prop = new PropertyImpl(OntManager.appendPrefix("#hasCepage"));
-        RDFNode node = vinIndividual.getPropertyValue(prop);
-        return node.toString();
+        return this.getProperty(cepage, "hasCepage");
+    }
+
+    public String getCru() {
+        return this.getProperty(cru, "hasCru");
+    }
+
+    public String getCuvee() {
+        return this.getProperty(cuvee, "hasCuvee");
+    }
+
+    public String getSol() {
+        return this.getProperty(sol, "hasSol");
+    }
+
+    public String getDegre() {
+        return this.getProperty(degre, "hasDegreValue");
+    }
+
+    private String getProperty(String property, String prefix) {
+        if(property == null) {
+            Property prop = new PropertyImpl(OntManager.appendPrefix("#" + prefix));
+            RDFNode node = vinIndividual.getPropertyValue(prop);
+
+            if(node != null) {
+                if(node.isResource()) {
+                    property = node.asResource().getLocalName();
+                }
+                else if(node.isLiteral()) {
+                    property = node.asLiteral().getString();
+                }
+                else if(node.isURIResource()) {
+                    property = node.toString();
+                }
+            }
+        }
+        return property;
     }
 }
